4248a62f744140889130baab5d5d8ad213cd2364
Asif <58527783+Asif-102@users.noreply.github.com>
2023-02-10 02:26:12 +0600

    added run command in comment

--- template.js
+++ template.js
@@ -18,7 +18,11 @@ process.stdin.on("end", () => {
 function readline() {
   return inputString[currentLine++];
 }
- 
+
+/* 
+$ cat input.txt | node main.js output on console.
+$ cat input.txt | node main.js > output.txt output in file.
+*/
 // ********** Code Start **********
 
 function main() {
